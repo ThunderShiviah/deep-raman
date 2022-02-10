72fe7e1c666eb930797584b156dbef33ffa112d4
Thunder Shiviah <thunder.shiviah@spiden.com>
2022-02-10 14:46:51 +0100

    add additional tests around model

--- tests/test_deep_raman.py
+++ tests/test_deep_raman.py
@@ -1,6 +1,17 @@
 import main
+import model
+from deep_raman import metrics
+
 import numpy as np
 
 def test_main_returns_noisy_raman():
     raman_array = main.main()
-    assert  isinstance(raman_array, np.ndarray), print(type(raman_array))
+    assert  isinstance(raman_array, np.ndarray), print(f"Expecting output of type np.ndarray but received {type(raman_array)}")
+
+def test_model_returns_correct_outputs():
+    NUM_EPOCHS = 1
+    LOSS_FUNCTION = metrics.psnr_loss
+    outputs = model.main(NUM_EPOCHS, LOSS_FUNCTION)
+
+    for output in outputs:
+        assert isinstance(output, np.ndarray), print(f"Expecting output of type np.ndarray but received {type(output)}")
